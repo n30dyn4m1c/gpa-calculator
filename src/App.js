bfc07ecfce5f012a45dd53215e76038d63c04f1f
= <=>
2024-09-24 17:31:28 +1000

    added courses.json and updated gpa calculator

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React, { useState } from 'react';
+import courses from './courses.json';  // Make sure the file path is correct
 
 const GPA_CALCULATOR = () => {
-  const [grades, setGrades] = useState({
-    course1: '',
-    course2: '',
-    course3: '',
-    course4: '',
-  });
-
+  const [grades, setGrades] = useState({});
   const [gpa, setGpa] = useState(null);
 
   const gradePoints = {
@@ -19,36 +14,26 @@ const GPA_CALCULATOR = () => {
     F: 0,
   };
 
-  // Assuming all courses have the same credit of 3
-  const courseCredits = {
-    course1: 3,
-    course2: 3,
-    course3: 3,
-    course4: 3,
-  };
-
-  const handleChange = (course, value) => {
+  const handleChange = (courseId, value) => {
     setGrades({
       ...grades,
-      [course]: value,
+      [courseId]: value,
     });
   };
 
-  // Updated GPA formula: GPA = Sum(Credit * GradePoint) / Sum(Credits)
   const calculateGpa = () => {
-    const totalPoints =
-      (courseCredits.course1 * gradePoints[grades.course1] || 0) +
-      (courseCredits.course2 * gradePoints[grades.course2] || 0) +
-      (courseCredits.course3 * gradePoints[grades.course3] || 0) +
-      (courseCredits.course4 * gradePoints[grades.course4] || 0);
+    let totalPoints = 0;
+    let totalCredits = 0;
 
-    const totalCredits =
-      courseCredits.course1 +
-      courseCredits.course2 +
-      courseCredits.course3 +
-      courseCredits.course4;
+    courses.forEach((course) => {
+      const grade = grades[course.id];
+      if (grade) {
+        totalPoints += course.credits * gradePoints[grade];
+        totalCredits += course.credits;
+      }
+    });
 
-    const calculatedGpa = totalPoints / totalCredits;
+    const calculatedGpa = totalCredits > 0 ? totalPoints / totalCredits : 0;
     setGpa(calculatedGpa.toFixed(2));
   };
 
@@ -56,81 +41,26 @@ const GPA_CALCULATOR = () => {
     <div style={{ padding: '20px', maxWidth: '600px', margin: '0 auto' }}>
       <h1>GPA Calculator</h1>
 
-      {/* Course 1 */}
-      <label>
-        Course 1 Grade:
-        <select
-          value={grades.course1}
-          onChange={(e) => handleChange('course1', e.target.value)}
-        >
-          <option value="">Select Grade</option>
-          <option value="HD">HD</option>
-          <option value="DI">DI</option>
-          <option value="CR">CR</option>
-          <option value="PA">PA</option>
-          <option value="CP">CP</option>
-          <option value="F">F</option>
-        </select>
-      </label>
-
-      <br />
-
-      {/* Course 2 */}
-      <label>
-        Course 2 Grade:
-        <select
-          value={grades.course2}
-          onChange={(e) => handleChange('course2', e.target.value)}
-        >
-          <option value="">Select Grade</option>
-          <option value="HD">HD</option>
-          <option value="DI">DI</option>
-          <option value="CR">CR</option>
-          <option value="PA">PA</option>
-          <option value="CP">CP</option>
-          <option value="F">F</option>
-        </select>
-      </label>
-
-      <br />
-
-      {/* Course 3 */}
-      <label>
-        Course 3 Grade:
-        <select
-          value={grades.course3}
-          onChange={(e) => handleChange('course3', e.target.value)}
-        >
-          <option value="">Select Grade</option>
-          <option value="HD">HD</option>
-          <option value="DI">DI</option>
-          <option value="CR">CR</option>
-          <option value="PA">PA</option>
-          <option value="CP">CP</option>
-          <option value="F">F</option>
-        </select>
-      </label>
-
-      <br />
-
-      {/* Course 4 */}
-      <label>
-        Course 4 Grade:
-        <select
-          value={grades.course4}
-          onChange={(e) => handleChange('course4', e.target.value)}
-        >
-          <option value="">Select Grade</option>
-          <option value="HD">HD</option>
-          <option value="DI">DI</option>
-          <option value="CR">CR</option>
-          <option value="PA">PA</option>
-          <option value="CP">CP</option>
-          <option value="F">F</option>
-        </select>
-      </label>
-
-      <br />
+      {courses.map((course) => (
+        <div key={course.id}>
+          <label>
+            {course.name} Grade:
+            <select
+              value={grades[course.id] || ''}
+              onChange={(e) => handleChange(course.id, e.target.value)}
+            >
+              <option value="">Select Grade</option>
+              <option value="HD">HD</option>
+              <option value="DI">DI</option>
+              <option value="CR">CR</option>
+              <option value="PA">PA</option>
+              <option value="CP">CP</option>
+              <option value="F">F</option>
+            </select>
+          </label>
+          <br />
+        </div>
+      ))}
 
       <button onClick={calculateGpa}>Calculate GPA</button>
 
